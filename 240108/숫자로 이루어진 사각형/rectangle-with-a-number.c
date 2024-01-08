1021680a4578def807abdd03af424fc42b891607
jeongwon23 <144980650+jeongwon23@users.noreply.github.com>
2024-01-08 19:29:20 +0900

    Title: 숫자로 이루어진 사각형, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240108/숫자로 이루어진 사각형/rectangle-with-a-number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void num(int n){
+    int value;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+           /* if (value > 9) {
+                value -= 9;
+            }*/
+            value = value % 9 + 1;
+            printf("%d ",value);
+        }printf("\n");
+    }
+}
+int main() {
+    int a;
+    scanf("%d",&a);
+    num(a);
+    return 0;
+}
