c0e524fcdc95c4e0b653ccc16d0537651a112733
jeongwon23 <144980650+jeongwon23@users.noreply.github.com>
2024-01-08 18:48:11 +0900

    Title: 반복 출력하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240108/반복 출력하기/repeated-output.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void PrintLines(int n){
+    for(int i=0;i<n;i++){
+        printf("12345^&*()_\n");
+    }
+}
+
+
+int main() {
+    int num = 4;
+    PrintLines(num);
+    return 0;
+}
