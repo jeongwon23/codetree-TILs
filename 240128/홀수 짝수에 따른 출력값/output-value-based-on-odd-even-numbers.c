a083d41b1e5605f4f4a5d19466697ab6cd479464
jeongwon23 <144980650+jeongwon23@users.noreply.github.com>
2024-01-28 19:02:51 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 1097ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240128/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.c
+++ 240128/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.c
@@ -1,34 +1,43 @@
 #include <stdio.h>
 
-// 홀수의 합을 계산하는 재귀 함수
+// 1부터 n까지의 홀수의 합을 계산하는 재귀 함수
 int sumOdd(int n) {
-    // 기저 사례: n이 1 이하일 때 0 반환
-    if (n <= 1)
-        return n;
+    // 기저 사례: n이 1이면 1 반환
+    if (n == 1)
+        return 1;
 
-    // 홀수인 경우 현재 값(n)과 이전 홀수까지의 합을 더함
-    return n + sumOdd(n - 2);
+    // n이 홀수인 경우 현재 값(n)과 이전 홀수까지의 합을 더함
+    if (n % 2 != 0)
+        return n + sumOdd(n - 2);
+    // n이 짝수인 경우 이전 값까지의 합을 반환
+    else
+        return sumOdd(n - 1);
 }
 
-// 짝수의 합을 계산하는 재귀 함수
+// 2부터 n까지의 짝수의 합을 계산하는 재귀 함수
 int sumEven(int n) {
-    // 기저 사례: n이 1 이하일 때 0 반환
-    if (n <= 1)
-        return 0;
+    // 기저 사례: n이 2면 2 반환
+    if (n == 2)
+        return 2;
 
-    // 홀수인 경우 현재 값(n-1)과 이전 짝수까지의 합을 더함
-    return n - 1 + sumEven(n - 2);
+    // n이 홀수인 경우 현재 값(n-1)과 이전 짝수까지의 합을 더함
+    if (n % 2 != 0)
+        return n - 1 + sumEven(n - 2);
+    // n이 짝수인 경우 이전 값까지의 합을 반환
+    else
+        return sumEven(n - 1);
 }
 
 int main() {
-    int n;
-    scanf("%d", &n);
+    // 사용자로부터 정수 N 입력 받기
+    int N;
+    scanf("%d", &N);
 
     // N이 홀수인 경우 홀수의 합 출력, 짝수인 경우 짝수의 합 출력
-    if (n % 2 != 0) {
-        printf("%d", sumOdd(n));
+    if (N % 2 != 0) {
+        printf("%d",sumOdd(N));
     } else {
-        printf("%d", sumEven(n));
+        printf("%d", sumEven(N));
     }
 
     return 0;
