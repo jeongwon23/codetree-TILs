e94c77feb9df1f64393a117a20b971443d16a312
jeongwon23 <144980650+jeongwon23@users.noreply.github.com>
2024-02-05 04:15:57 +0900

    Title: 폭탄 해제, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240205/폭탄 해제/bomb-removal.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef struct {
+    char code[10];
+    char color[5];
+    int second;
+} Person;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    Person p1;
+    scanf("%s %s %d", p1.code, p1.color, &p1.second);
+
+    printf("code : %s\n", p1.code);
+    printf("color : %s\n", p1.color);
+    printf("second : %d\n", p1.second);
+
+    return 0;
+}
