fe12a2042fa603a857b704db9082a786632205ce
jeongwon23 <144980650+jeongwon23@users.noreply.github.com>
2024-02-05 04:21:27 +0900

    Title: 폭탄 해제, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240205/폭탄 해제/bomb-removal.c
+++ 240205/폭탄 해제/bomb-removal.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
     char code[10];
-    char color[2];
+    char *color;
     int second;
 } Person;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
     Person p1;
+
+    // 동적 메모리 할당을 통해 color에 필요한 크기만큼의 공간을 할당받음
+    p1.color = (char *)malloc(20 * sizeof(char)); // 예시로 최대 20글자로 가정
+
     scanf("%s %s %d", p1.code, p1.color, &p1.second);
 
     printf("code : %s\n", p1.code);
     printf("color : %s\n", p1.color);
     printf("second : %d\n", p1.second);
+
+    // 할당 받은 동적 메모리 해제
+    free(p1.color);
 
     return 0;
 }
