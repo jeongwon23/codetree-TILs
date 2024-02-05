bd5fa01c69dad862121d96d8ec131a405256cf6f
jeongwon23 <144980650+jeongwon23@users.noreply.github.com>
2024-02-05 18:09:22 +0900

    Title: 재귀함수를 이용한 별 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240205/재귀함수를 이용한 별 출력/star-output-with-recursive-function.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int func(int n){
+    if (n == 0) {
+        return 0;
+    } 
+    func(n - 1);
+    
+    for(int i=1;i<=n;i++){
+        printf("*");
+       
+    } printf("\n");
+        
+    
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+    scanf("%d",&n);
+    func(n);
+    return 0;
+}
