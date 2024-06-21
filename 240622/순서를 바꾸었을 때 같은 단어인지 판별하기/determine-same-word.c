e1b0475f047e51279e25442df19df00724faa778
jeongwon23 <144980650+jeongwon23@users.noreply.github.com>
2024-06-22 02:46:18 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 13ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240622/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.c
+++ 240622/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.c
@@ -1,42 +1,27 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-// 함수: 두 단어가 애너그램인지 확인
-int areAnagrams(char *str1, char *str2) {
-    int count1[256] = {0}; // ASCII 문자 빈도수
-    int count2[256] = {0}; // ASCII 문자 빈도수
-
-    // 첫 번째 단어의 문자 빈도수 세기
-    for (int i = 0; str1[i] && str2[i]; i++) {
-        count1[(unsigned char)str1[i]]++;
-        count2[(unsigned char)str2[i]]++;
-    }
-
-    // 두 문자열의 길이가 다른 경우
-    if (strlen(str1) != strlen(str2))
-        return 0;
-
-    // 두 단어의 문자 빈도수 비교
-    for (int i = 0; i < 256; i++) {
-        if (count1[i] != count2[i])
-            return 0;
-    }
-
-    return 1;
+// 비교 함수
+int compare(const void *a, const void *b) {
+    return (*(char *)a - *(char *)b);
 }
 
 int main() {
     char str1[100];
     char str2[100];
 
-    scanf("%s\n", str1);
+    scanf("%s %s", str1, str2);
 
-    scanf("%s", str2);
+    // 각 문자열을 정렬합니다.
+    qsort(str1, strlen(str1), sizeof(char), compare);
+    qsort(str2, strlen(str2), sizeof(char), compare);
 
-    if (areAnagrams(str1, str2))
-        printf("Yes");
+    // 정렬된 문자열 비교
+    if(strcmp(str1, str2) == 0)
+        printf("Yes\n");
     else
-        printf("No");
+        printf("No\n");
 
     return 0;
 }
